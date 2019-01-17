b1169f518cb510b2c9b60fc1b07f0aa6214c2ec5
Grayson112233 <graysonpike@gmail.com>
2019-01-17 02:04:57 -0600

    Only update every hour

--- main.py
+++ main.py
@@ -50,12 +50,13 @@ def main():
                     info.rain_enabled = not info.rain_enabled
                 if event.key == K_RETURN:
                     graphics.toggle_fullscreen()
-        if last_minute != datetime.now().minute:
+        now = datetime.now()
+        if last_minute != now.minute and (now.minute == 0 or last_minute == -1):
             info.weather = get_weather(info.woeid, info.yahoo_client_key, info.yahoo_client_secret)
             sleep(0.01)
             info.time_string = strftime("%-I:%M")
             info.date_string = calendar.day_name[date.today().weekday()] + ", " + calendar.month_abbr[date.today().month] + " " + ordinal(date.today().day)
-            last_minute = datetime.now().minute
+            last_minute = now.minute
         graphics.render(info)
 
 
